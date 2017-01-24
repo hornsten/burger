f7b17c30fe4672606bbbab8b49f6d7998aced50c
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-01-24 11:29:44 -0600

    Add JAWSDB to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 //In this file is everything needed to establish connection with MySQL db
 var mysql = require("mysql");
 var password = require("../password");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: password,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: password,
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {
     if (err) {

7a3fb25632e6bfc5472a1d59ecd57568a178ae31
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-01-26 10:45:10 -0600

    commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-// var password = require("./password");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
